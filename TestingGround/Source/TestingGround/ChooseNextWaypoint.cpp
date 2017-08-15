7a9372ca05cd1750696ee3999ac9095853e3bf23
Mazen Morgan <morgan.mazen42@gmail.com>
2017-08-15 17:32:10 +0200

    Composition Over Inheritance

--- TestingGround/Source/TestingGround/ChooseNextWaypoint.cpp
+++ TestingGround/Source/TestingGround/ChooseNextWaypoint.cpp
@@ -3,23 +3,25 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"	//TODO remove coupling
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index)
-
-	//Get Patrol Points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrollingPoints = PatrollingGuard->PatrolPoints;
+	//Get Patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { EBTNodeResult::Failed; }
+	auto PatrollingPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrollingPoints.Num() == 0 ) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Patrolling Points Assigned!"))
+		EBTNodeResult::Failed;
+	}
 
 	//Set New Waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrollingPoints[Index]);
-	//TODO protected empty points.
 
 	//Cycle Index
 	auto NextIndex = (Index + 1) % (PatrollingPoints.Num());
